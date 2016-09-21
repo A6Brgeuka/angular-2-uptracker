7a2d13766fb7608232d4912f1149416c51ba8493
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-09-21 19:07:20 +0300

    view fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,6 @@ export interface AppConfig {
   apiEndpoint: string;
 }
 
-export let qqq = {
-  qqq:111
-}
-
 export const APP_DI_CONFIG: AppConfig = {
   // apiEndpoint: process.env.API_URL,
   // apiEndpoint: 'http://private-anon-ce8323ff87-uptracker.apiary-mock.com/api/v1'
