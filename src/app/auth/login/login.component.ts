e79edfe8818bd9007578d7ac41c57bc303faaa39
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-10-13 19:05:04 +0300

    phone input component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CookieService } from 'angular2-cookie/services';
 
-import { UserModel } from '../../models/index';
 import { UserService, SpinnerService } from '../../core/services/index';
 
 @Component({
@@ -10,7 +8,7 @@ import { UserService, SpinnerService } from '../../core/services/index';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   loginUser = {
     email: '',
     password: ''
@@ -19,7 +17,6 @@ export class LoginComponent implements OnInit {
   constructor(
       private userService: UserService,
       private router: Router,
-      private cookieService: CookieService,
       private spinnerService: SpinnerService
   ) {
     if (!this.userService.isGuest()){
@@ -27,27 +24,22 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-
-  }
-
   onSubmit() {
     this.spinnerService.show();
     this.userService.login(this.loginUser)
         .subscribe(
             (res: any) => {
-              // this.spinnerService.hide();
-
               // Get the redirect URL from service
               // If no redirect has been set, use the default
               let redirect = this.userService.redirectUrl ? this.userService.redirectUrl : '/dashboard';
+
               // check for passing signup steps for navigation
               let signupStep = this.userService.currentSignupStep();
               switch(signupStep) {
-                case 2:  this.router.navigate(['/signup', 'about-company']); return;
-                case 3:  this.router.navigate(['/signup', 'payment-info']); return;
-                case 4:  this.router.navigate(['/']); return;
-                default:this.router.navigate([redirect]);
+                case 2:   this.router.navigate(['/signup', 'about-company']); return;
+                case 3:   this.router.navigate(['/signup', 'payment-info']); return;
+                case 4:   this.router.navigate(['/']); return;
+                default:  this.router.navigate([redirect]);
               }
             }
         );
--- src/app/auth/signup/create-account/create-account.component.ts
+++ src/app/auth/signup/create-account/create-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'angular2-cookie/services';
 
@@ -13,13 +13,13 @@ import { UserService, SpinnerService } from '../../../core/services/index';
 export class CreateAccountComponent implements OnInit {
   signupAccount: UserModel;
   public phoneMask = [/\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/ ];
-  inernationalPhoneMask = [ /\+/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/ ];
-  usPhoneMask = ['+', '1', ' ', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/ ];
+  // usPhoneMask = ['+', '1', ' ', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/ ];
   terms: boolean = false;
   privacy: boolean = false;
 
   signupFormPhone: string = '';
   selectedCountry: any = [];
+  temp;
 
   constructor(
       private userService: UserService,
@@ -31,12 +31,23 @@ export class CreateAccountComponent implements OnInit {
 
   ngOnInit() {
     this.signupAccount = this.userService.selfData || new UserModel();
+    
+    // default country for phone input
+    this.selectedCountry = [ "United States", "us", "1", 0 ];
+  }
+
+  onCountryChange($event) {
+    // TODO: change phone mask dynamically if necessary
+    // this.phoneMask = [ /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/ ];
+    // let codeArr = country[2].split('');
+    // codeArr.unshift('+');
+    // codeArr.push(' ', ' ');
+    // this.phoneMask = codeArr.concat(this.phoneMask);
+    this.selectedCountry = $event;
   }
 
   onSubmit(){
-    // TODO:
-    // when phone mask is ready, add country code to phone ngModel
-    // this.signupAccount.phone = this.selectedCountry[2] + ' ' + this.signupFormPhone;
+    this.signupAccount.phone = this.selectedCountry[2] + ' ' + this.signupFormPhone;
     this.spinnerService.show();
     this.userService.signUp(this.signupAccount)
         .subscribe(
