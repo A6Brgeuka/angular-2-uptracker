32664b3bfb5cbc2a00f1f0f37d51a241337dd9a6
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-10-21 18:29:24 +0300

    onboard accounting view

--- src/app/onboard/accounting/accounting.component.ts
+++ src/app/onboard/accounting/accounting.component.ts
@@ -12,7 +12,11 @@ import { UserService, AccountService } from '../../core/services/index';
 @DestroySubscribers()
 export class AccountingComponent implements OnInit {
   public locationArr: any = [];
-  private subscribers: any = {};
+  public accounting: any = {};
+  public currencyArr: any;
+  public currencyDirty: boolean = false;
+  public currencySign: string ='$';
+  public disabledRange: any = [];
 
   constructor(
       private router: Router,
@@ -27,13 +31,28 @@ export class AccountingComponent implements OnInit {
     //     this.locationArr = res.account.locations;
     //   }
     // });
+    this.disabledRange = [true, false];
+    this.accounting.total = [ 300000, 100000 ];
+
+
+
+    // materialize="pickadate"
+    //     [materializeParams]="[{selectMonths: true, selectYears: 15}]"
+  }
+
+  changeCurrency(){
+    this.currencyDirty = true;
+  }
+
+  toggleLock(i){
+    this.disabledRange[i] = !this.disabledRange[i]; 
   }
 
   goBack(){
     this.router.navigate(['/onboard','users']);
   }
 
-  goNext(){
+  onSubmit(){
     
   }
 
