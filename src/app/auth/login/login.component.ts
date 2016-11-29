735daada57dd435f99e3851e24292ed4080d6b31
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-11-29 14:13:03 +0200

    login form send request condition for form validity

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,7 +21,11 @@ export class LoginComponent {
   ) {
   }
 
-  onSubmit() {
+  onSubmit(event) {
+    if (!event.target.checkValidity()){
+      return;
+    }
+
     this.userService.login(this.loginUser)
         .subscribe(
             (res: any) => {
