522c3ea12bcb991b8bc16c75431b854336d59525
Dmitriy Melnichenko <tairezzzz@gmail.com>
2017-03-13 17:55:41 +0200

    updated cli version

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -2,19 +2,19 @@ import { AuthComponent } from './auth.component';
 import { AuthGuard } from '../auth-guard.service';
 
 import { EmailVerificationRoutes } from './email-verification/email-verification.routing';
+import { NoContentComponent } from '../no-content/no-content.component';
 
 export const AuthRoutes = [
   {
     path: '',
     component: AuthComponent,
-    resolve: {},
     children: [
-      { path: 'signup', canLoad: [AuthGuard], loadChildren: './signup/signup.module#SignupModule' },
       { path: 'login', canLoad: [AuthGuard], loadChildren: './login/login.module#LoginModule' },
+      { path: 'signup', canLoad: [AuthGuard], loadChildren: './signup/signup.module#SignupModule' },
       { path: 'forgot-password', canLoad: [AuthGuard], loadChildren: './forgot-password/forgot-password.module#ForgotPasswordModule' },
       { path: 'forgot-password-congrats', loadChildren: './forgot-password-congrats/forgot-password-congrats.module#ForgotPasswordCongratsModule' },
       { path: 'reset-password/:token', loadChildren: './reset-password/reset-password.module#ResetPasswordModule' },
       ...EmailVerificationRoutes
     ]
-  }
+  },
 ];
