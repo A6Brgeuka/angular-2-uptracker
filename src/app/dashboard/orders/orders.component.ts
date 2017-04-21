9f3aeb69e9306798e0e1477bda73fd477346b69a
tairezzzz <tairezzzz@gmail.com>
2017-04-21 16:58:03 +0300

    orders v1

--- src/app/dashboard/orders/orders.component.ts
+++ src/app/dashboard/orders/orders.component.ts
@@ -6,10 +6,6 @@ import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
 import { DestroySubscribers } from 'ng2-destroy-subscribers';
 import * as _ from 'lodash';
 
-// import { ViewProductModal } from './view-product-modal/view-product-modal.component';
-// import { EditProductModal } from './edit-product-modal/edit-product-modal.component';
-import { UserService, AccountService } from '../../core/services/index';
-
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -20,65 +16,20 @@ export class OrdersComponent implements OnInit {
   public searchKey$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   public sortBy: string;
   public sortBy$: BehaviorSubject<any> = new BehaviorSubject(null);
-  public total: number;
+  public total: number = 6;
   public products$: Observable<any>;
-
+  items = [0,0,0,0,0,0,0,0,0,0];
+  
   constructor(
       public modal: Modal
   ) {
   }
 
   ngOnInit() {
-    this.products$ = Observable.of([
-      { name: 'First', vendor_name: 'vendor1'},
-      { name: 'Second', vendor_name: 'vendor2'},
-    ])
-    // this.vendors$ = Observable
-    //     .combineLatest(
-    //         // this.vendorService.combinedVendors$,
-    //         this.sortBy$,
-    //         this.searchKey$
-    //     )
-    //     .map(([vendors, sortBy, searchKey]) => {  
-    //       this.total = vendors.length;
-    //       let filteredVendors = vendors;
-    //       if (searchKey && searchKey!='') {
-    //         filteredVendors = _.reject(filteredVendors, (vendor: any) =>{
-    //           let key = new RegExp(searchKey, 'i');
-    //           return !key.test(vendor.name);
-    //         });
-    //       }
-    //       let order = 'desc';
-    //       if (sortBy == 'A-Z') {
-    //         sortBy = 'name';
-    //         order = 'asc';
-    //       }
-    //       if (sortBy == 'Z-A') {
-    //         sortBy = 'name';
-    //       }
-    //      
-    //       let sortedVendors = _.orderBy(filteredVendors, [sortBy], [order]);
-    //       return sortedVendors;
-    //     });
+  
   }
 
-  viewProductModal(product){
-    // this.modal
-    //     .open(ViewProductModal,  overlayConfigFactory({ product: product }, BSModalContext))
-    //     .then((resultPromise)=>{
-    //       resultPromise.result.then(
-    //           (res) => {
-    //             this.editProductModal(res);
-    //           },
-    //           (err)=>{}
-    //       );
-    //     });
-  }
-
-  editProductModal(product = null){
-    // this.modal.open(EditProductModal,  overlayConfigFactory({ product: product }, BSModalContext));
-  }
-
+  
   searchFilter(event){
     // replace forbidden characters
     let value = event.target.value.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
