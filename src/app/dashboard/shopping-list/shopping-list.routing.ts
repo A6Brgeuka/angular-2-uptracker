6aeac26b3978839e1e33dc588762ae90e7b3c557
Dmitriy Melnichenko <tairezzzz@gmail.com>
2017-05-02 17:53:02 +0300

    aot, build in prod shopping list bug

--- src/app/dashboard/shopping-list/shopping-list.routing.ts
+++ src/app/dashboard/shopping-list/shopping-list.routing.ts
@@ -1,14 +1,11 @@
 import { ShoppingListComponent } from './shopping-list.component';
 
-import { CurrencyCollectionResolve } from '../../shared/resolves/index'
-import { OrdersPreviewRoutes } from './orders-preview/orders-preview.routing';
 
 export const ShoppingListRoutes = [
   {
     path: 'shoppinglist',
     component: ShoppingListComponent,
     resolve: {
-      CurrencyCollectionResolve
     },
     children: [
     ],
