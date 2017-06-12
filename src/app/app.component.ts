ce441d614c33bf2f6a8d784845468d02bc50b986
tairezzzz <tairezzzz@gmail.com>
2017-06-12 11:17:44 +0300

    User Feedback Module Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,32 @@ export class AppComponent implements OnInit{
       public activatedRoute: ActivatedRoute,
       public userService: UserService
   ) {
+  
+    // userecho service config
+    window['_ues'] = {
+      host: 'kirawoods.userecho.com',
+      forum: '1',
+      lang: 'en',
+      tab_corner_radius: 5,
+      tab_font_size: 20,
+      tab_image_hash: 'ZmVlZGJhY2s%3D',
+      tab_chat_hash: 'Y2hhdA%3D%3D',
+      tab_alignment: 'right',
+      tab_text_color: '#ffffff',
+      tab_text_shadow_color: '#00000055',
+      tab_bg_color: '#57a957',
+      tab_hover_color: '#f45c5c'
+    };
   }
 
-  ngOnInit(){ 
+  ngOnInit(){
+    // userecho service
+    let node = document.createElement('script');
+    node.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+    node.type = 'text/javascript';
+    node.async = true;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,56 +1,54 @@
-import {Component, OnInit, NgZone} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 import { DestroySubscribers } from 'ng2-destroy-subscribers';
 
 import { UserService, StateService, AccountService } from '../core/services/index';
-import {ActivatedRoute} from "@angular/router";
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
-  styleUrls: [ './dashboard.style.scss' ],
+  styleUrls: ['./dashboard.style.scss'],
   templateUrl: './dashboard.template.html'
 })
 @DestroySubscribers()
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   public subscribers: any = {};
   public selectedLocation: string = '';
   public locations$: any;
   public locationArr: any;
-  public isnotProductUrl$: Observable<any>;
-
+  
   constructor(
-      public userService: UserService,
-      public accountService: AccountService,
-      public stateService: StateService,
+    public userService: UserService,
+    public accountService: AccountService,
+    public stateService: StateService,
   ) {
-    this.locations$ =  Observable.combineLatest(
-        this.accountService.locations$,
-        this.stateService.navigationEndUrl$
+    this.locations$ = Observable.combineLatest(
+      this.accountService.locations$,
+      this.stateService.navigationEndUrl$
     )
-        .map(([locations,navigationEndUrl]): any => locations)
-        .do((res: any) => {
-          this.locationArr = res;
-          console.log('location arr',res);
-        });
-
+    .map(([locations, navigationEndUrl]): any => locations)
+    .do((res: any) => {
+      this.locationArr = res;
+      console.log('location arr', res);
+    });
+    
+    
   }
   
-  ngOnInit(){
-
+  ngOnInit() {
+    
     this.subscribers.dashboardLocationSubscription = this.accountService.dashboardLocation$.subscribe((res: any) => {
       this.selectedLocation = res ? res.id : '';
     });
     this.subscribers.dashboardLocationProductSubscription = this.accountService.locations$
-        .subscribe( res => {
-          this.locationArr=res;
-          //this.accountService.dashboardLocation$.next(res[0])
-        });
+    .subscribe(res => {
+      this.locationArr = res;
+    });
+    
   }
-
-  changeLocation(event){
+  
+  changeLocation(event) {
     this.accountService.dashboardLocation$.next(_.find(this.locationArr, {'id': event.target.value}));
   }
-
+  
 }
