17088098eada24aa4765ab83dd78f0859a267481
tairezzzz <tairezzzz@gmail.com>
2018-01-08 18:55:15 +0200

    Order List Updates

--- src/app/dashboard/orders/backordered-list/backordered-list.component.ts
+++ src/app/dashboard/orders/backordered-list/backordered-list.component.ts
@@ -11,6 +11,36 @@ import { DestroySubscribers } from 'ng2-destroy-subscribers';
 export class BackorderedListComponent implements OnInit, OnDestroy {
   public subscribers: any = {};
   
+  public orders: any[] = [
+    {
+      id: '3',
+      order_number: 'AMT-0001',
+      product_name: 'Some Product Name',
+      location: 'Primary Location',
+      status: 'Pending',
+      placed: '7/5/18',
+      backordered: '8/5/18',
+      qty: '100',
+      pkg_price: '$1.00',
+      total: '$100.00',
+      flagged: true,
+      favorite: true,
+    },
+    {
+      id: '4',
+      order_number: 'AMT-0002',
+      product_name: 'Some Product Name',
+      location: 'Primary Location',
+      status: 'Pending',
+      placed: '7/5/18',
+      backordered: '8/5/18',
+      qty: '10',
+      pkg_price: '$1.00',
+      total: '$10.00',
+      flagged: false,
+      favorite: false,
+    },
+  ];
   constructor(
   
   ) {
--- src/app/dashboard/orders/open-list/open-orders-list.component.ts
+++ src/app/dashboard/orders/open-list/open-orders-list.component.ts
@@ -11,6 +11,37 @@ import { DestroySubscribers } from 'ng2-destroy-subscribers';
 export class OpenOrdersListComponent implements OnInit, OnDestroy {
   public subscribers: any = {};
   
+  public orders: any[] = [
+    {
+      id: '5',
+      order_number: 'AMT-0001',
+      product_name: 'Some Product Name',
+      location: 'Primary Location',
+      status: 'Pending',
+      placed: '7/5/18',
+      backordered: '8/5/18',
+      qty: '100',
+      pkg_price: '$1.00',
+      total: '$100.00',
+      flagged: true,
+      favorite: true,
+    },
+    {
+      id: '6',
+      order_number: 'AMT-0002',
+      product_name: 'Some Product Name',
+      location: 'Primary Location',
+      status: 'Pending',
+      placed: '7/5/18',
+      backordered: '8/5/18',
+      qty: '10',
+      pkg_price: '$1.00',
+      total: '$10.00',
+      flagged: false,
+      favorite: false,
+    },
+  ];
+  
   constructor(
   
   ) {
