27fd176fa7cd513a46efa6c7fcf2c99d6be797d4
tairezzzz <tairezzzz@gmail.com>
2018-02-08 18:50:32 +0200

    feat(dashboard:orders:receive:models): add new fields to order-form model

--- src/app/dashboard/orders/receive/models/order-form.model.ts
+++ src/app/dashboard/orders/receive/models/order-form.model.ts
@@ -1,15 +1,18 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { OrderItemFormGroup, OrderItemFormModel } from './order-item-form.model';
 
 export interface OrderFormModel {
   order_id: string;
-  items: any;
+  items: OrderItemFormModel[];
 }
 
 export class OrderFormGroup extends FormGroup {
 
   constructor({ order_id, items }: OrderFormModel) {
+    const itemsGroups = items.map((item) => new OrderItemFormGroup(item));
     super({
       order_id: new FormControl(order_id, Validators.required),
+      items: new FormArray(itemsGroups),
     });
   }
 }
