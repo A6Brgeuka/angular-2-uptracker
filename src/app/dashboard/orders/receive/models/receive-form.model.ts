d1dd93461029ea41c1fd6ae3b4a0ebc7b94dc6ef
tairezzzz <tairezzzz@gmail.com>
2018-02-08 18:51:09 +0200

    feat(dashboard:orders:receive:models): add new fields to receive-form model

--- src/app/dashboard/orders/receive/models/receive-form.model.ts
+++ src/app/dashboard/orders/receive/models/receive-form.model.ts
@@ -1,18 +1,21 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { OrderFormGroup, OrderFormModel } from './order-form.model';
 
 export interface ReceiveFormModel {
   invoice_number?: string;
   packing_slip_number?: string;
-  orders: any[];
+  orders: OrderFormModel[];
   vendor: {vendor_name: string, vendor_id: string};
 }
 
 export class ReceiveFormGroup extends FormGroup {
 
   constructor({ orders, vendor }: ReceiveFormModel) {
+    const ordersGroups = orders.map((order) => new OrderFormGroup(order));
     super({
       invoice_number: new FormControl(),
       packing_slip_number: new FormControl('', Validators.required),
+      orders: new FormArray(ordersGroups),
       vendor: new FormGroup({
         vendor_id: new FormControl(vendor.vendor_id, Validators.required),
         vendor_name: new FormControl(vendor.vendor_name, Validators.required),
