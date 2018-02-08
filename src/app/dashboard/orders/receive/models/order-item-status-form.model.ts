b0aa19b5db19bdeb331b3ffa09910c46cdc76b24
tairezzzz <tairezzzz@gmail.com>
2018-02-08 18:42:31 +0200

    feat(dashboard:orders:receive:models): add new fields

--- src/app/dashboard/orders/receive/models/order-item-status-form.model.ts
+++ src/app/dashboard/orders/receive/models/order-item-status-form.model.ts
@@ -11,17 +11,36 @@ export interface OrderItemStatusFormModel {
   primary_status: boolean;
   location_id: string;
   storage_location_id: string;
+  status?: string;
+  inventory_group_name?: string;
+  location_name?: string;
+  storage_location_name?: string;
 }
 
 export class OrderItemStatusFormGroup extends FormGroup {
 
-  constructor({ type, qty,  primary_status, location_id, storage_location_id }: OrderItemStatusFormModel) {
+  constructor(
+    { type,
+      qty,
+      primary_status = false,
+      location_id,
+      storage_location_id,
+      status,
+      inventory_group_name,
+      location_name,
+      storage_location_name,
+    }: OrderItemStatusFormModel
+  ) {
     super({
-      type: new FormControl('', Validators.required),
-      qty: new FormControl('', Validators.required),
-      primary_status: new FormControl(false, Validators.required),
-      location_id: new FormControl('', Validators.required),
-      storage_location_id: new FormControl('', Validators.required),
+      type: new FormControl(type, Validators.required),
+      qty: new FormControl(qty, Validators.required),
+      primary_status: new FormControl(primary_status, Validators.required),
+      location_id: new FormControl(location_id, Validators.required),
+      storage_location_id: new FormControl(storage_location_id, Validators.required),
+      status: new FormControl(status),
+      inventory_group_name: new FormControl(inventory_group_name),
+      location_name: new FormControl(location_name),
+      storage_location_name: new FormControl(storage_location_name),
     });
   }
 }
