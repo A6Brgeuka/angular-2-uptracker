70e294b719d8baed0a1446df8e3babc0274fd4fa
tairezzzz <tairezzzz@gmail.com>
2018-02-09 19:19:39 +0200

    feat(dashboard:orders:receive/models): add optional fields to order-item-form-model

--- src/app/dashboard/orders/receive/models/order-item-form.model.ts
+++ src/app/dashboard/orders/receive/models/order-item-form.model.ts
@@ -3,23 +3,27 @@ import { OrderItemStatusFormGroup, OrderItemStatusFormModel } from './order-item
 
 import * as _ from 'lodash';
 
-export interface OrderItemFormModel {
-  item_id: string;
+export interface ReceiveOrderItemModel {
+  id: string;
   status?: OrderItemStatusFormModel[];
   status_line_items?: OrderItemStatusFormModel[];
   inventory_group_id: string;
+  quantity?: number;
+  item_name?: string;
+  location_name?: string;
+  location_id?: string;
 }
 
 export class OrderItemFormGroup extends FormGroup {
 
-  constructor({ item_id, inventory_group_id, status_line_items}: OrderItemFormModel) {
+  constructor({ id, inventory_group_id, status_line_items}: ReceiveOrderItemModel) {
     const isStatusLineItemsArray = status_line_items && _.isArray(status_line_items);
     const statusLineItemsFormGroups =
       isStatusLineItemsArray ?
         status_line_items.map(item => new OrderItemStatusFormGroup(item)) : [];
 
     super({
-      item_id: new FormControl(item_id, Validators.required),
+      item_id: new FormControl(id, Validators.required),
       inventory_group_id: new FormControl(inventory_group_id, Validators.required),
       status: new FormArray([]),
       status_line_items: new FormArray(statusLineItemsFormGroups),
