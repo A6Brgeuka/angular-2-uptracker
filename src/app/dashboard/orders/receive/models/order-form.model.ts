ba5b1d2411bea6113d9a807f43b06720d0bd0919
tairezzzz <tairezzzz@gmail.com>
2018-02-09 18:49:47 +0200

    fix(dashboard:orders:receive:models): add optional field po_number and change name of interface in order-form model

--- src/app/dashboard/orders/receive/models/order-form.model.ts
+++ src/app/dashboard/orders/receive/models/order-form.model.ts
@@ -1,14 +1,15 @@
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { OrderItemFormGroup, OrderItemFormModel } from './order-item-form.model';
+import { OrderItemFormGroup, ReceiveOrderItemModel } from './order-item-form.model';
 
-export interface OrderFormModel {
+export interface ReceiveOrderModel {
   order_id: string;
-  items: OrderItemFormModel[];
+  po_number?: string;
+  items: ReceiveOrderItemModel[];
 }
 
 export class OrderFormGroup extends FormGroup {
 
-  constructor({ order_id, items }: OrderFormModel) {
+  constructor({ order_id, items }: ReceiveOrderModel) {
     const itemsGroups = items.map((item) => new OrderItemFormGroup(item));
     super({
       order_id: new FormControl(order_id, Validators.required),
