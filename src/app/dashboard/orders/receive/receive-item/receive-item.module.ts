b83c158cb7a90d6f54f609db51ff5daaa9dda314
tairezzzz <tairezzzz@gmail.com>
2018-02-09 19:55:21 +0200

    feat(dashboard:orders:receive:receive-item): start to change receive-item component

--- src/app/dashboard/orders/receive/receive-item/receive-item.module.ts
+++ src/app/dashboard/orders/receive/receive-item/receive-item.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { AppSharedModule } from '../../../../shared/shared.module';
 
 import { ReceiveItemComponent } from './receive-item.component';
+import { ReceiveStatusLineItemModule } from './receive-status-line-item/receive-status-line-item.module';
+import { ReceiveNewStatusItemModule } from './receive-new-status-item/receive-new-status-item.module';
 
 @NgModule({
   declarations: [ReceiveItemComponent],
   exports: [ReceiveItemComponent],
-  imports: [AppSharedModule],
+  imports: [
+    AppSharedModule,
+    ReceiveStatusLineItemModule,
+    ReceiveNewStatusItemModule,
+  ],
   providers: []
 })
 export class ReceiveItemModule {
