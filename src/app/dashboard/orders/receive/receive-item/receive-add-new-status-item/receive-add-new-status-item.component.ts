6e216ef333fec305f8ed6dfab5aff3dc45a28ef5
tairezzzz <tairezzzz@gmail.com>
2018-02-12 23:37:01 +0200

    feat(orders:receive-item): update recieve add status cmp

--- src/app/dashboard/orders/receive/receive-item/receive-add-new-status-item/receive-add-new-status-item.component.ts
+++ src/app/dashboard/orders/receive/receive-item/receive-add-new-status-item/receive-add-new-status-item.component.ts
@@ -1,6 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { DestroySubscribers } from 'ng2-destroy-subscribers';
+
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+import * as _ from 'lodash';
+
+import { ReceivedOrderService } from '../../../../../core/services';
+
+import { OrderReceivingStatus } from '../../models/order-item-status-form.model';
 
 @Component({
   selector: 'app-receive-add-new-status-item',
@@ -8,7 +17,40 @@ import { DestroySubscribers } from 'ng2-destroy-subscribers';
 })
 @DestroySubscribers()
 
-export class ReceiveAddNewStatusItemComponent  {
+export class ReceiveAddNewStatusItemComponent implements OnInit {
 
+  public statusList$: Observable<OrderReceivingStatus[]> = this.receivedOrderService.statusList$;
+  public newStatusList$: ReplaySubject<OrderReceivingStatus[]> = new ReplaySubject<OrderReceivingStatus[]>(1);
+  public remainingStatusList$: Observable<OrderReceivingStatus[]>;
 
+  @Input() qty: number;
+
+  @Input()
+  set selected(value: OrderReceivingStatus[]) {
+    this.newStatusList$.next(value);
+  }
+
+  @Output() statusAdd: EventEmitter<{type: string, qty: number}> = new EventEmitter();
+
+  constructor(
+    private receivedOrderService: ReceivedOrderService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.remainingStatusList$ = Observable.combineLatest(
+      this.statusList$,
+      this.newStatusList$,
+    )
+    .map(([statusList, newStatusList]) => _.differenceWith(statusList, newStatusList, _.isEqual));
+  }
+
+  onListSelect(type) {
+    if (type !== 'pending') {
+      this.statusAdd.emit({
+        type,
+        qty: this.qty,
+      });
+    }
+  }
 }
