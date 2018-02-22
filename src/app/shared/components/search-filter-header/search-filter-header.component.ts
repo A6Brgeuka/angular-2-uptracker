6c70b14c99406df875176d9f7aecb57526c622a7
tairezzzz <tairezzzz@gmail.com>
2018-02-22 20:11:37 +0200

    refactor(shared:components:search-filter-header): refactor search filter event

--- src/app/shared/components/search-filter-header/search-filter-header.component.ts
+++ src/app/shared/components/search-filter-header/search-filter-header.component.ts
@@ -1,27 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export type SearchType = 'keyword' | 'chips' | 'multiple';
+
+
 @Component({
   selector: 'app-search-filter-header',
   templateUrl: './search-filter-header.component.html',
   styleUrls: ['./search-filter-header.component.scss'],
 })
-
 export class SearchFilterHeaderComponent {
-  
+
   @Input() public title:  string;
   @Input() public searchKey: string;
+  @Input() public searchType: SearchType = 'keyword';
   @Output() searchEvent = new EventEmitter();
   @Output() resetEvent = new EventEmitter();
   @Output() openModalEvent = new EventEmitter();
 
-  constructor(
-
-  ) {
-  
-  }
-
   searchFilter(event) {
-    this.searchEvent.emit(event.data);
+    this.searchKey = event;
+    this.searchEvent.emit(event);
   }
 
   showFiltersModal() {
