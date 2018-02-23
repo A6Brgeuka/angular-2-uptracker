8935e37c3b3b90ea106e7fba8135db0fdf6298ac
Adam Sumner <adamsumner@gmail.com>
2018-02-23 03:37:13 -0800

    Lots of CSS fixes and correct restock/on-floor behavior

--- src/app/core/services/restock.service.ts
+++ src/app/core/services/restock.service.ts
@@ -27,6 +27,26 @@ export class RestockService extends ModelService {
 
   onInit() {
     this.selfData$ = this.restangular.all('restock').customGET()
-      .map((res: any) => res.data.locations)
+      .map(
+        (res: any) => res.data.locations.map(
+          (location: any) => ({
+            id: location.id,
+            name: location.name,
+            floorstock_locations: location.floorstock_locations.map(
+              (floorstock_location: any) => ({
+                id: floorstock_location.id,
+                name: floorstock_location.name,
+                inventory_groups: floorstock_location.inventory_groups.map(
+                  (inventory_group: any) => ({
+                    ...inventory_group,
+                    on_floor_qty: '',
+                    restock_qty: ''
+                  })
+                )
+              })
+            )
+          })
+        )
+      )
   }
 }
