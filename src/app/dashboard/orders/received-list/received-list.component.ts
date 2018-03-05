b246b7bb79b7338b846f959aec9edacf3f44cb71
tairezzzz <tairezzzz@gmail.com>
2018-03-05 12:36:53 +0200

    feat(orders:received-list): add alias array in qty field

--- src/app/dashboard/orders/received-list/received-list.component.ts
+++ src/app/dashboard/orders/received-list/received-list.component.ts
@@ -25,7 +25,7 @@ export class ReceivedListComponent implements OnInit, OnDestroy {
     {name: 'Placed', className: 's1', alias: 'placed_date', filterBy: true, },
     {name: 'Received', className: 's1', alias: 'received_date', filterBy: true, },
     {name: 'Reconciled', className: 's1', alias: 'reconciled_date', filterBy: true, },
-    {name: 'Qty', className: 's1 bold underline-text right-align', alias: 'quantity'},
+    {name: 'Qty', className: 's1 bold underline-text right-align', aliasArray: ['received_quantity', 'quantity'], join: '/'},
     {name: 'Pkg Price', className: 's1', alias: 'package_price'},
     {name: 'Total', className: 's1 bold underline-text right-align', alias: 'total'},
     {name: '', className: 's1', actions: true},
