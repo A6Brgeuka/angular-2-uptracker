bac1a3347bda755d07913c397b3d322131ed8a1b
tairezzzz <tairezzzz@gmail.com>
2018-03-06 15:47:38 +0200

    feat(orders:models): add order list type const

---
+++ src/app/dashboard/orders/models/order-list-type.ts
@@ -0,0 +1,9 @@
+export const OrderListType = {
+  all: 'all',
+  open: 'open',
+  received: 'received',
+  backordered: 'backordered',
+  reconciled: 'reconciled',
+  flagged: 'flagged',
+  favorited: 'favorited',
+};
