0771aa693333a750f21e1efaf65d030f51d0693e
phoenix377 <phoenix.number377@gmail.com>
2018-03-26 04:34:35 +0300

    fix(vendor:add-vendor): Several bug fixes for adding vendor 1. Implement orderByDate Pipe 2. Update total count updates when new vendor is added

--- src/app/shared/modals/add-vendor-modal/add-vendor-modal.component.ts
+++ src/app/shared/modals/add-vendor-modal/add-vendor-modal.component.ts
@@ -131,7 +131,7 @@ export class AddVendorModalComponent implements OnInit {
 
     this.vendorService.addAccountVendor(this.formData)
       .do(res => this.vendorService.addToCollection$.next(res))
-      .subscribe(res => this.router.navigate(['/vendors/edit/' + res.id]) && this.dismissModal())
+      .subscribe(res => this.router.navigate(['/vendors/edit/' + res.id]) && this.closeModal(true))
   }
 
   uploadLogo(file: any) {
