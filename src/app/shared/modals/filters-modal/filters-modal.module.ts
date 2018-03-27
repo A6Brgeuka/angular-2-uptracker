6ade218091058b87bfb4872e405e08b602878276
tairezzzz <tairezzzz@gmail.com>
2018-03-27 19:15:47 +0300

    fix(shared:modals:filters-modal): move styles to global, remove different filters modals

--- src/app/shared/modals/filters-modal/filters-modal.module.ts
+++ src/app/shared/modals/filters-modal/filters-modal.module.ts
@@ -3,20 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FiltersModalComponent } from './filters-modal.component';
-import { OrdersPageFiltersModule } from './orders-page-filters/orders-page-filters.module';
-import { MarketplaceFiltersModule } from './marketplace-filters/marketplace-filters.module';
-import { InventoryGroupFiltersModule } from './inventory-group-filters/inventory-group-filters.module';
-import { ShoppingListFiltersModule } from './shopping-list-filters/shopping-list-filters.module';
+// import { MarketplaceFiltersModule } from './marketplace-filters/marketplace-filters.module';
+// import { InventoryGroupFiltersModule } from './inventory-group-filters/inventory-group-filters.module';
+// import { ShoppingListFiltersModule } from './shopping-list-filters/shopping-list-filters.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MarketplaceFiltersModule,
-    OrdersPageFiltersModule,
-    InventoryGroupFiltersModule,
-    ShoppingListFiltersModule,
+    // MarketplaceFiltersModule,
+    // InventoryGroupFiltersModule,
+    // ShoppingListFiltersModule,
   ],
   declarations: [
     FiltersModalComponent,
