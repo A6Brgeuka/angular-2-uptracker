da0d5a4e498dfa4979930191d9223e5567a7700b
tairezzzz <tairezzzz@gmail.com>
2018-03-29 18:37:06 +0300

    feat(shared/modals/filters-modal/orders-page-filters): move vendors filter to separate component

--- src/app/shared/modals/filters-modal/orders-page-filters/orders-page-filters.component.ts
+++ src/app/shared/modals/filters-modal/orders-page-filters/orders-page-filters.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
-import { DestroySubscribers } from 'ngx-destroy-subscribers';
-import * as _ from 'lodash';
-
-import { VendorService } from '../../../../core/services/vendor.service';
 import { BSModalContext } from 'angular2-modal/plugins/bootstrap';
 import { DialogRef, ModalComponent } from 'angular2-modal';
-import { AllOrdersListService } from '../../../../dashboard/orders/services/all-orders-list.service';
 import { PastOrderService } from '../../../../core/services/pastOrder.service';
 import { OrdersPageFiltersModel } from './orders-page-filters.model';
 
@@ -19,38 +14,21 @@ export class OrdersPageFiltersModalContext extends BSModalContext {
   selector: 'app-orders-page-filters',
   templateUrl: './orders-page-filters.component.html',
 })
-@DestroySubscribers()
 export class OrdersPageFiltersComponent implements OnInit, ModalComponent<OrdersPageFiltersModalContext> {
-  private subscribers: any = {};
-  public vendors$;
-  public vendorsData: any[];
   public context;
-
   public filterForm: FormGroup;
-
-  public vendorsCollection = {};
-  public autocompleteVendors = {
-    autocompleteOptions: {
-      data: this.vendorsCollection,
-      limit: Infinity,
-      minLength: 0,
-    }
-  };
 
   constructor(
     public dialog: DialogRef<OrdersPageFiltersModalContext>,
     public pastOrderService: PastOrderService,
-    private vendorService: VendorService,
   ) {
     this.context = dialog.context;
-    this.vendors$ = this.vendorService.getVendors();
 
     this.filterForm = new FormGroup({
       archived: new FormControl(),
       bogo: new FormControl(),
       percent_off: new FormControl(),
       rewards: new FormControl(),
-      vendorsName: new FormControl(),
       vendors: new FormControl(),
       voided: new FormControl(),
       my_favorite: new FormControl(),
@@ -77,9 +55,6 @@ export class OrdersPageFiltersComponent implements OnInit, ModalComponent<Orders
   get vendorsControl() {
     return this.filterForm.get('vendors');
   }
-  get vendorsNameControl() {
-    return this.filterForm.get('vendorsName');
-  }
   get myFavoriteControl() {
     return this.filterForm.get('my_favorite');
   }
@@ -91,22 +66,7 @@ export class OrdersPageFiltersComponent implements OnInit, ModalComponent<Orders
   }
 
   ngOnInit() {
-    this.subscribers.getVendorsSubscription = this.vendors$
-    .subscribe((res: any) => {
-      this.vendorsData = _.flatten(res.data.vendors);
-      this.vendorsData.map((vendor: any) => {
-        this.vendorsCollection[vendor.name] = null;
-      });
-    });
 
-    this.subscribers.changeVendorsValueSubscription = this.vendorsNameControl.valueChanges
-    .subscribe((vendors: string[]) => {
-      const VendorsIds = vendors.map((item) => {
-        const vendorId = _.find(this.vendorsData, ['name', item]);
-        return vendorId.id;
-      });
-      this.vendorsControl.setValue(VendorsIds);
-    });
   }
 
   dismissModal() {
