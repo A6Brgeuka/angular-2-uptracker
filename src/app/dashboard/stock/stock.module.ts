852667a2c12361eaccf520843bf10c10bf13f495
Ulises <ulises@Ulises.local>
2018-03-29 13:10:10 -0400

    Add sort pipe

--- src/app/dashboard/stock/stock.module.ts
+++ src/app/dashboard/stock/stock.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { StockComponent } from './stock.component';
 import { AppSharedModule } from '../../shared/shared.module';
 import { UpdateStockModalModule } from './update-stock-modal/update-stock-modal.module';
+import { SortProductsPipe } from './stock.pipe';
 
 @NgModule({
   declarations: [
-    StockComponent
+    StockComponent,
+    SortProductsPipe
   ],
   imports: [
     AppSharedModule,
