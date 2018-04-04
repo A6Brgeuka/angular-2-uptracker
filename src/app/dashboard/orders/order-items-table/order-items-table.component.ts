90df3b0f9aa2b31b8aa6250585c256411f776819
tairezzzz <tairezzzz@gmail.com>
2018-04-04 19:10:33 +0300

    feat(orders:order-items-table): move streams with chips to service

--- src/app/dashboard/orders/order-items-table/order-items-table.component.ts
+++ src/app/dashboard/orders/order-items-table/order-items-table.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { DestroySubscribers } from 'ngx-destroy-subscribers';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
 import { Modal } from 'angular2-modal';
 
-import { map, filter, intersectionBy } from 'lodash';
+import { map } from 'lodash';
 
-// import { OrdersPageFiltersComponent } from '../../../shared/modals/filters-modal/orders-page-filters/orders-page-filters.component';
 import { PastOrderService } from '../../../core/services/pastOrder.service';
 import { ModalWindowService } from '../../../core/services/modal-window.service';
 import { ToasterService } from '../../../core/services/toaster.service';
-import { OrderTableResetService } from '../directives/order-table/order-table-reset.service';
-import { OrderTableFilterByService } from '../directives/order-table/order-table-filter-by.service';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-order-items-table',
@@ -22,13 +18,9 @@ import { OrderTableFilterByService } from '../directives/order-table/order-table
   styleUrls: ['./order-items-table.component.scss']
 })
 @DestroySubscribers()
-export class OrderItemsTableComponent implements OnInit, OnDestroy {
+export class OrderItemsTableComponent  {
   public subscribers: any = {};
-  public searchKey$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   public orders$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  public searchKey: string;
-  public sortBy: string;
-  public sortBy$: BehaviorSubject<any> = new BehaviorSubject(null);
   public visible: boolean[] = [];
 
   public orderTabs = {
@@ -44,81 +36,18 @@ export class OrderItemsTableComponent implements OnInit, OnDestroy {
 
   public orderTabsArr = map(this.orderTabs, (value, key) => value);
 
-  private activeChange$ = new Subject<{active: boolean, tab: string}>();
-  private activeTab$: Observable<string>;
-
-  public chips$;
-
-  private filterItems$: Observable<any[]>;
-  private onChipsChange$ = new Subject<string[]>();
-
   constructor(
     public modal: Modal,
     public router: Router,
     public pastOrderService: PastOrderService,
     public modalWindowService: ModalWindowService,
     public toasterService: ToasterService,
-    public orderTableResetService: OrderTableResetService,
-    private orderTableFilterByService: OrderTableFilterByService
+    private ordersService: OrdersService,
   ) {
   }
 
-  ngOnInit() {
-    this.activeTab$ = this.activeChange$
-    .filter((event) => event.active)
-    .map((event) => event.tab)
-    .shareReplay(1);
-
-    this.filterItems$ = this.activeTab$
-    .switchMap((tab) =>
-      this.orderTableFilterByService.getFilterByListName(tab)
-    )
-    .map((filterObj) => map(filterObj, (value, key) => ({value, key})))
-    .map((items) => filter(items, 'value'))
-    .shareReplay(1);
-
-    this.chips$ = this.filterItems$
-    .map((items) => map(items, (item) => item.value));
-
-  }
-
-  ngOnDestroy() {
-    console.log('for unsubscribing');
-  }
-
-  addSubscribers() {
-    this.onChipsChange$
-    .withLatestFrom(this.filterItems$, this.activeTab$)
-    .subscribe(([chips, filterItems, tab]) => {
-      const chipsObj = chips.map((value) => ({value}));
-      const items = intersectionBy(filterItems, chipsObj, 'value');
-      const filter = items.reduce((acc, item) => ({...acc, [item.key]: item.value}), {});
-      this.orderTableFilterByService.setFilterBy(filter, tab);
-    });
-  }
-
-  onChipChange(chips) {
-    this.onChipsChange$.next(chips);
-  }
-
-  searchOrders(event) {
-    // replace forbidden characters
-    const value = event.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-    this.searchKey$.next(value);
-  };
-
-  // showFiltersModal() {
-  //   this.modal
-  //   .open(OrdersPageFiltersComponent, this.modalWindowService.overlayConfigFactoryWithParams({}));
-  // }
-  //
-  // resetFilters() {
-  //   this.pastOrderService.filterQueryParams$.next(null);
-  //   this.orderTableResetService.resetFilters();
-  // }
-
   activeChange(active: boolean, tab: string) {
-    this.activeChange$.next({active, tab});
+    this.ordersService.activeChange$.next({active, tab});
   }
 
   changeDataType(event) {
