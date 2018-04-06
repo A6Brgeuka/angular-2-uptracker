1b17a7287641b4ad6eab7fd37cd8c3adf18cadcb
tairezzzz <tairezzzz@gmail.com>
2018-04-06 10:45:54 +0300

    feat(orders:o-t:services): add OrdersTableService to index file

--- src/app/dashboard/orders/orders-table/services/index.ts
+++ src/app/dashboard/orders/orders-table/services/index.ts
@@ -1,8 +1,10 @@
 import { AllOrdersListService } from './all-orders-list.service';
 import { OpenOrdersListService } from './open-orders-list.service';
 import { ReceivedOrdersListService } from './received-orders-list.service';
+import { OrdersTableService } from './orders-table.service';
 
 export const ORDERS_PROVIDERS = [
+  OrdersTableService,
   AllOrdersListService,
   OpenOrdersListService,
   ReceivedOrdersListService,
