ae7b8476acd415fe070bd8ae703c1d96022a730a
tairezzzz <tairezzzz@gmail.com>
2018-04-06 12:55:28 +0300

    fix(core:services): remove unnecessary subject and commented code

--- src/app/core/services/pastOrder.service.ts
+++ src/app/core/services/pastOrder.service.ts
@@ -3,69 +3,23 @@ import { Router } from '@angular/router';
 
 import { Restangular } from 'ngx-restangular';
 
-import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
-import { ConnectableObservable } from 'rxjs/observable/ConnectableObservable';
 
 @Injectable()
 export class PastOrderService {
 
-  // public entities$: ConnectableObservable<{ [id: string]: any }>;
-
   public sortBy$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
   public filterBy$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
-
-  // public voidOrder$ = new Subject();
-  // public voidOrderRequest$: ConnectableObservable<any>;
-
-  // public removeIds$;
-  // private addCollectionToEntittesStream$: Subject<Observable<any>> = new Subject();
-  public filterQueryParams$: Subject<any> = new Subject();
 
   constructor(
     public restangular: Restangular,
     public router: Router,
   ) {
 
-    // this.voidOrderRequest$ = this.voidOrder$
-    // .switchMap((data) =>
-    //   this.restangular.one('pos', 'void').customPOST(data)
-    //   .map(res => res.data)
-    // ).publish();
-    // this.voidOrderRequest$.connect();
-
-    // this.removeIds$ = this.voidOrderRequest$
-    // .map((voidedOrders) => voidedOrders.map((order) => order.id));
-    //
-    // this.addCollectionStreamToEntittesStream(this.voidOrderRequest$);
-    //
-    // this.entities$ = this.addCollectionToEntittesStream$
-    // .mergeAll()
-    // .scan((acc, items: any[]) => {
-    //   const newEntities = items.reduce((itemEntities, item) => {
-    //     const oldEntity = acc[item.id];
-    //     const entityToSet = oldEntity ? {...oldEntity, ...item} : item;
-    //     return {
-    //       ...itemEntities,
-    //       [item.id]: entityToSet,
-    //     };
-    //   }, {});
-    //
-    //   return {...acc, ...newEntities};
-    // }, {})
-    // .publishReplay(1);
-    // this.entities$.connect();
   }
 
   reorder(data) {
     return this.restangular.all('reorder').customPOST(data);
-  }
-
-  onVoidOrder(data) {
-    // this.voidOrder$.next(data);
-    // return this.voidOrderRequest$;
-    return null;
   }
 
   updateSortBy(param) {
@@ -85,13 +39,5 @@ export class PastOrderService {
   goToReceive(params, type?) {
     this.router.navigate(['orders/receive', params], {queryParams: {type}});
   }
-
-  /**
-   * Used to add stream as source for entities
-   * @param {Observable<any>} stream$
-   */
-  // public addCollectionStreamToEntittesStream(stream$: Observable<any>) {
-  //   this.addCollectionToEntittesStream$.next(stream$);
-  // }
 
 }
