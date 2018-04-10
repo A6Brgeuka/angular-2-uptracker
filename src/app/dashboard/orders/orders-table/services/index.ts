23f89d243b213bc94f2395c949b6172a562d3885
tairezzzz <tairezzzz@gmail.com>
2018-04-10 14:19:18 +0300

    feat(orders/orders-table/services): add service for getting favorited orders collection

--- src/app/dashboard/orders/orders-table/services/index.ts
+++ src/app/dashboard/orders/orders-table/services/index.ts
@@ -1,6 +1,7 @@
 import { AllOrdersListService } from './all-orders-list.service';
 import { BackorderedOrdersListService } from './backordered-orders-list.service';
 import { ClosedOrdersListService } from './closed-orders-list.service';
+import { FavoritedOrdersListService } from './favorited-orders-list.service';
 import { OpenOrdersListService } from './open-orders-list.service';
 import { OrdersTableService } from './orders-table.service';
 import { ReceivedOrdersListService } from './received-orders-list.service';
@@ -11,6 +12,7 @@ export const ORDERS_PROVIDERS = [
   AllOrdersListService,
   BackorderedOrdersListService,
   ClosedOrdersListService,
+  FavoritedOrdersListService,
   OpenOrdersListService,
   ReceivedOrdersListService,
   ReconciledOrdersListService,
