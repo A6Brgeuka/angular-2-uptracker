ec972f00bd910f63936cb8a0cf952def8847b6bd
tairezzzz <tairezzzz@gmail.com>
2018-04-10 14:48:49 +0300

    feat(orders/orders-table): create service for getting flagged orders collection

--- src/app/dashboard/orders/orders-table/services/index.ts
+++ src/app/dashboard/orders/orders-table/services/index.ts
@@ -2,6 +2,7 @@ import { AllOrdersListService } from './all-orders-list.service';
 import { BackorderedOrdersListService } from './backordered-orders-list.service';
 import { ClosedOrdersListService } from './closed-orders-list.service';
 import { FavoritedOrdersListService } from './favorited-orders-list.service';
+import { FlaggedOrdersListService } from './flagged-orders-list.service';
 import { OpenOrdersListService } from './open-orders-list.service';
 import { OrdersTableService } from './orders-table.service';
 import { ReceivedOrdersListService } from './received-orders-list.service';
@@ -13,6 +14,7 @@ export const ORDERS_PROVIDERS = [
   BackorderedOrdersListService,
   ClosedOrdersListService,
   FavoritedOrdersListService,
+  FlaggedOrdersListService,
   OpenOrdersListService,
   ReceivedOrdersListService,
   ReconciledOrdersListService,
