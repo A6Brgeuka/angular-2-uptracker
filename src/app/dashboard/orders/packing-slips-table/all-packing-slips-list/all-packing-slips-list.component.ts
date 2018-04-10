a4ef208b03e71ce0b437769d3c66b0ad35457aaf
tairezzzz <tairezzzz@gmail.com>
2018-04-10 19:27:15 +0300

    feat(orders/packing-slips-table/all-packing-slips-list): create module for all packing slips list

---
+++ src/app/dashboard/orders/packing-slips-table/all-packing-slips-list/all-packing-slips-list.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { DestroySubscribers } from 'ngx-destroy-subscribers';
+
+import { Observable } from 'rxjs/Observable';
+
+import { OrderListType } from '../../models/order-list-type';
+import { OrderItem } from '../../models/order-item';
+import { PastOrderService } from '../../../../core/services/pastOrder.service';
+import { AllPackingSlipsListService } from '../services/all-packing-slips-list.service';
+
+@Component({
+  selector: 'app-all-packing-slips-list',
+  templateUrl: './all-packing-slips-list.component.html',
+  styleUrls: ['./all-packing-slips-list.component.scss'],
+})
+@DestroySubscribers()
+export class AllPackingSlipsListComponent implements OnInit, OnDestroy {
+  public subscribers: any = {};
+
+  public listName: string = OrderListType.all;
+  public tableHeader: any = [
+    {name: 'Packing Slip #', className: 's1', alias: 'order_number', filterBy: true, },
+    {name: 'Vendor', className: 's2', alias: 'vendor_name', filterBy: true, wrap: 2, },
+    {name: 'Status', className: 's1', alias: 'status', filterBy: true, showChevron: true, },
+    {name: 'Location', className: 's1', alias: 'location_name', filterBy: true, },
+    // {name: 'Placed', className: 's1', alias: 'placed_date', filterBy: true, },
+    // {name: 'Received', className: 's1', alias: 'received_date', filterBy: true, },
+    // {name: 'Reconciled', className: 's1', alias: 'reconciled_date', filterBy: true, },
+    // {name: '# of Items', className: 's2 items-numbers', aliasItemArray: 'order_items'},
+    // {name: 'Total', className: 's1 bold underline-text right-align', alias: 'total'},
+    // {name: '', className: 's1', actions: true},
+  ];
+
+  public packingSlips$: Observable<OrderItem[]>;
+
+  constructor(
+    public pastOrderService: PastOrderService,
+    public allPackingSlipsListService: AllPackingSlipsListService,
+  ) {
+
+  };
+
+  ngOnInit() {
+    this.packingSlips$ = this.allPackingSlipsListService.collection$;
+  };
+
+  addSubscribers() {
+    // this.subscribers.getAllCollectionSubscription = this.allPackingSlipsListService.getCollection()
+    // .subscribe();
+  };
+
+  ngOnDestroy() {
+    console.log('for unsubscribing');
+  };
+
+  sortByHeaderUpdated(event) {
+    this.pastOrderService.updateSortBy(event.alias);
+  }
+
+  onFilterBy(value) {
+    this.pastOrderService.updateFilterBy(value);
+  }
+
+}
