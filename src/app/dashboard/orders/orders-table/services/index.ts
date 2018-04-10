7f67af3cb3cbf86d51672cd7cfe105a6556c7d24
tairezzzz <tairezzzz@gmail.com>
2018-04-10 13:15:24 +0300

    feat(orders/orders-table/services): create service for getting closed orders collection

--- src/app/dashboard/orders/orders-table/services/index.ts
+++ src/app/dashboard/orders/orders-table/services/index.ts
@@ -1,5 +1,6 @@
 import { AllOrdersListService } from './all-orders-list.service';
 import { BackorderedOrdersListService } from './backordered-orders-list.service';
+import { ClosedOrdersListService } from './closed-orders-list.service';
 import { OpenOrdersListService } from './open-orders-list.service';
 import { OrdersTableService } from './orders-table.service';
 import { ReceivedOrdersListService } from './received-orders-list.service';
@@ -9,6 +10,7 @@ export const ORDERS_PROVIDERS = [
   OrdersTableService,
   AllOrdersListService,
   BackorderedOrdersListService,
+  ClosedOrdersListService,
   OpenOrdersListService,
   ReceivedOrdersListService,
   ReconciledOrdersListService,
