0b5c3f576708aa1a858bec4b07a73786bfe2f935
tairezzzz <tairezzzz@gmail.com>
2018-04-11 10:58:10 +0300

    feat(orders/packing-slips-table/services): create service for getting open packing slips collection

--- src/app/dashboard/orders/packing-slips-table/services/index.ts
+++ src/app/dashboard/orders/packing-slips-table/services/index.ts
@@ -1,7 +1,9 @@
 import { AllPackingSlipsListService } from './all-packing-slips-list.service';
 import { PackingSlipsTableService } from './packing-slips-table.service';
+import { OpenPackingSlipsListService } from './open-packing-slips-list.service';
 
 export const PACKINGSLIPS_PROVIDERS = [
   AllPackingSlipsListService,
+  OpenPackingSlipsListService,
   PackingSlipsTableService,
 ];
---
+++ src/app/dashboard/orders/packing-slips-table/services/open-packing-slips-list.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Restangular } from 'ngx-restangular';
+
+import { OrderListBaseService } from '../../classes/order-list-base.service';
+import { OrdersService } from '../../orders.service';
+import { PackingSlipsTableService } from './packing-slips-table.service';
+
+@Injectable()
+export class OpenPackingSlipsListService extends OrderListBaseService {
+
+  protected idName = 'id';
+
+  constructor(
+    private restangular: Restangular,
+    private packingSlipsTableService: PackingSlipsTableService,
+    private ordersService: OrdersService,
+  ) {
+    super(packingSlipsTableService, ordersService);
+    this.packingSlipsTableService.addCollectionStreamToEntittesStream(this.getCollectionRequest$);
+  }
+
+  getRequest(params) {
+    return this.restangular.one('packingslips', 'open').customGET('', params);
+  }
+}
