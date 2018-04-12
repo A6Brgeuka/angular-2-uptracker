2eae58e306b597dc503d1c9df9a9abee2c47f70d
tairezzzz <tairezzzz@gmail.com>
2018-04-12 18:40:34 +0300

    feat(orders/invoices-table/services): create service for getting open invoices collection

--- src/app/dashboard/orders/invoices-table/services/index.ts
+++ src/app/dashboard/orders/invoices-table/services/index.ts
@@ -1,7 +1,9 @@
 import { AllInvoicesListService } from './all-invoices-list.service';
 import { InvoicesTableService } from './invoices-table.service';
+import { OpenInvoicesListService } from './open-invoices-list.service';
 
 export const INVOICES_PROVIDERS = [
   AllInvoicesListService,
   InvoicesTableService,
+  OpenInvoicesListService,
 ];
