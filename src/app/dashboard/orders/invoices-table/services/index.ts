85fd234e93bb3105a45c08282f4fb25707601249
tairezzzz <tairezzzz@gmail.com>
2018-04-12 19:21:23 +0300

    feat(orders/invoices-table/services): create service for getting pending approval invoices collection

--- src/app/dashboard/orders/invoices-table/services/index.ts
+++ src/app/dashboard/orders/invoices-table/services/index.ts
@@ -1,8 +1,10 @@
+import { ApprovalInvoicesListService } from './approval-invoices-list.service';
 import { AllInvoicesListService } from './all-invoices-list.service';
 import { InvoicesTableService } from './invoices-table.service';
 import { OpenInvoicesListService } from './open-invoices-list.service';
 
 export const INVOICES_PROVIDERS = [
+  ApprovalInvoicesListService,
   AllInvoicesListService,
   InvoicesTableService,
   OpenInvoicesListService,
