708323a7e18a90e0d78235c42be279df2b8d94b5
ulisesorozco <ulisesiorozco@gmail.com>
2018-04-12 11:30:49 +0800

    Add basic bulk board

--- src/app/dashboard/orders/reconcile/reconcile.component.ts
+++ src/app/dashboard/orders/reconcile/reconcile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DestroySubscribers } from 'ngx-destroy-subscribers';
 import * as moment from 'moment';
+import { any } from 'ramda'
 
 @Component({
   selector: 'app-reconcile',
@@ -25,9 +26,42 @@ export class ReconcileComponent implements OnInit, OnDestroy {
   }
   public sort: string = 'A-Z';
   public filter: string = '';
+  public panelVisible: boolean = false
+  public products: Array<any> = [];
   
   constructor() {
-  
+    this.products = [
+      {
+        name: 'Gloves Tender Touch Nitrile Sempecare',
+        pkgTop: 'Box', pkgBottom: 'Box',
+        qtyTop: 100, qtyBottom: 50,
+        pkgpTop: '$10.00', pkgpBottom: '$10.00',
+        dtTop: '0.00', dtBottom: '$5.00',
+        discTop: '$20.00', discBottom: '$15.00',
+        totalTop: '$500.00', totalBottom: '$450.00',
+        checked: false,
+      },
+      {
+        name: 'Gloves Tender Touch Nitrile Sempecare',
+        pkgTop: 'Box', pkgBottom: 'Box',
+        qtyTop: 100, qtyBottom: 50,
+        pkgpTop: '$10.00', pkgpBottom: '$10.00',
+        dtTop: '0.00', dtBottom: '$5.00',
+        discTop: '$20.00', discBottom: '$15.00',
+        totalTop: '$500.00', totalBottom: '$450.00',
+        checked: false,
+      },
+      {
+        name: 'Gloves Tender Touch Nitrile Sempecare',
+        pkgTop: 'Box', pkgBottom: 'Box',
+        qtyTop: 100, qtyBottom: 50,
+        pkgpTop: '$10.00', pkgpBottom: '$10.00',
+        dtTop: '0.00', dtBottom: '$5.00',
+        discTop: '$20.00', discBottom: '$15.00',
+        totalTop: '$500.00', totalBottom: '$450.00',
+        checked: false,
+      }
+    ]
   }
   
   ngOnInit() {
@@ -59,6 +93,19 @@ export class ReconcileComponent implements OnInit, OnDestroy {
   }
 
   filterChange() {
-    
+
+  }
+
+  productSelect(product) {
+    product.checked = !product.checked
+    this.panelVisible = any((pd) => pd.checked)(this.products);
+  }
+
+  bulkUpdates() {
+    this.panelVisible = false;
+  }
+
+  bulkNevermind() {
+    this.panelVisible = false;
   }
 }
