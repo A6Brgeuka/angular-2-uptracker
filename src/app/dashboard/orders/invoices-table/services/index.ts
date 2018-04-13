d109c727c54b527322a0c41361bc4d02718cd7de
tairezzzz <tairezzzz@gmail.com>
2018-04-13 10:42:36 +0300

    feat(orders/invoices-table/services): create service for getting pending payment invoices collection

--- src/app/dashboard/orders/invoices-table/services/index.ts
+++ src/app/dashboard/orders/invoices-table/services/index.ts
@@ -2,10 +2,12 @@ import { ApprovalInvoicesListService } from './approval-invoices-list.service';
 import { AllInvoicesListService } from './all-invoices-list.service';
 import { InvoicesTableService } from './invoices-table.service';
 import { OpenInvoicesListService } from './open-invoices-list.service';
+import { PaymentInvoicesListService } from './payment-invoices-list.service';
 
 export const INVOICES_PROVIDERS = [
   ApprovalInvoicesListService,
   AllInvoicesListService,
   InvoicesTableService,
   OpenInvoicesListService,
+  PaymentInvoicesListService,
 ];
