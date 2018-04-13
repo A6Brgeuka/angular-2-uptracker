7f4e12e17ccdaedc189c87b8111707d470913cf8
tairezzzz <tairezzzz@gmail.com>
2018-04-13 11:06:42 +0300

    feat(orders/invoices-table/services): create service for getting flagged invoices collection

--- src/app/dashboard/orders/invoices-table/services/index.ts
+++ src/app/dashboard/orders/invoices-table/services/index.ts
@@ -1,6 +1,7 @@
 import { ApprovalInvoicesListService } from './approval-invoices-list.service';
 import { AllInvoicesListService } from './all-invoices-list.service';
 import { InvoicesTableService } from './invoices-table.service';
+import { FlaggedInvoicesListService } from './flagged-invoices-list.service';
 import { OpenInvoicesListService } from './open-invoices-list.service';
 import { PaymentInvoicesListService } from './payment-invoices-list.service';
 import { PaidInvoicesListService } from './paid-invoices-list.service';
@@ -9,6 +10,7 @@ export const INVOICES_PROVIDERS = [
   ApprovalInvoicesListService,
   AllInvoicesListService,
   InvoicesTableService,
+  FlaggedInvoicesListService,
   OpenInvoicesListService,
   PaymentInvoicesListService,
   PaidInvoicesListService,
