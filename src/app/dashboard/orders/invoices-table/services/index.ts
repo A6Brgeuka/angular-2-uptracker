9cf0ab0622e720cd74a73a47ab2f69e7b5968bda
tairezzzz <tairezzzz@gmail.com>
2018-04-13 10:54:34 +0300

    feat(orders/invoices-table/services): create service for getting paid invoices collection

--- src/app/dashboard/orders/invoices-table/services/index.ts
+++ src/app/dashboard/orders/invoices-table/services/index.ts
@@ -3,6 +3,7 @@ import { AllInvoicesListService } from './all-invoices-list.service';
 import { InvoicesTableService } from './invoices-table.service';
 import { OpenInvoicesListService } from './open-invoices-list.service';
 import { PaymentInvoicesListService } from './payment-invoices-list.service';
+import { PaidInvoicesListService } from './paid-invoices-list.service';
 
 export const INVOICES_PROVIDERS = [
   ApprovalInvoicesListService,
@@ -10,4 +11,5 @@ export const INVOICES_PROVIDERS = [
   InvoicesTableService,
   OpenInvoicesListService,
   PaymentInvoicesListService,
+  PaidInvoicesListService,
 ];
