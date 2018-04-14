07b847447463652b593ad29b1030d36189ab54c1
ulisesorozco <ulisesiorozco@gmail.com>
2018-04-15 03:13:09 +0800

    Initial reconcile api integration

--- src/app/dashboard/orders/reconcile/reconcile.component.ts
+++ src/app/dashboard/orders/reconcile/reconcile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DestroySubscribers } from 'ngx-destroy-subscribers';
 import * as moment from 'moment';
 import { any } from 'ramda'
+import { ReconcileService } from '../../../core/services/reconcile.service';
 
 @Component({
   selector: 'app-reconcile',
@@ -27,10 +28,8 @@ export class ReconcileComponent implements OnInit, OnDestroy {
   public sort: string = 'A-Z';
   public filter: string = '';
   public panelVisible: boolean = false
-  public products: Array<any> = [];
   public invoices: Array<any> = [];
   public invoices_: Array<any> = [];
-  public invoiceId: string = '';
   public selectedInvoice: any = {};
   public DOLLARSIGNS: any = {
     USD: '$',
@@ -39,95 +38,11 @@ export class ReconcileComponent implements OnInit, OnDestroy {
     JPY: '¥',
   }
   public board: any = {}
-  public selectConfig = { displayKey: "id", search: true };
+  public selectConfig = { displayKey: "order_number", search: true };
 
-  constructor() {
-    this.products = [
-      {
-        name: 'Gloves Tender Touch Nitrile Sempecare 1',
-        pkgTop: 'Box', pkgBottom: 'Box',
-        qtyTop: 100, qtyBottom: 50,
-        pkgpTop: 10.00, pkgpBottom: 10.00,
-        dtTop: 0.00, dtBottom: 5.00,
-        discTop: 20.00, discBottom: 15.00,
-        totalTop: 500.00, totalBottom: 450.00,
-        checked: false,
-      },
-      {
-        name: 'Gloves Tender Touch Nitrile Sempecare 2',
-        pkgTop: 'Box', pkgBottom: 'Box',
-        qtyTop: 100, qtyBottom: 50,
-        pkgpTop: 10.00, pkgpBottom: 10.00,
-        dtTop: 0.00, dtBottom: 5.00,
-        discTop: 20.00, discBottom: 15.00,
-        totalTop: 500.00, totalBottom: 450.00,
-        checked: false,
-      },
-      {
-        name: 'Gloves Tender Touch Nitrile Sempecare 3',
-        pkgTop: 'Box', pkgBottom: 'Box',
-        qtyTop: 100, qtyBottom: 50,
-        pkgpTop: 10.00, pkgpBottom: 10.00,
-        dtTop: 0.00, dtBottom: 5.00,
-        discTop: 20.00, discBottom: 15.00,
-        totalTop: 500.00, totalBottom: 450.00,
-        checked: false,
-      },
-      {
-        name: 'Gloves Tender Touch Nitrile Sempecare 4',
-        pkgTop: 'Box', pkgBottom: 'Box',
-        qtyTop: 100, qtyBottom: 50,
-        pkgpTop: 10.00, pkgpBottom: 10.00,
-        dtTop: 0.00, dtBottom: 5.00,
-        discTop: 20.00, discBottom: 15.00,
-        totalTop: 500.00, totalBottom: 450.00,
-        checked: false,
-      }
-    ]
-    this.invoices = [
-      {
-        id: '#KOM-123456787',
-        date: 'Feb 1, 2018',
-        currency: 'USD',
-        calculatedSubTotal: 250,
-        invoicedSubTotal: 220,
-        invoiceCredit: 5,
-        shipping: 20,
-        handling: 5,
-        taxes: 0,
-        total: 200,
-        discountAmount: 20,
-        discountType: 'PERCENT',
-      },
-      {
-        id: '#KOM-123456788',
-        date: 'Feb 2, 2018',
-        currency: 'CAD',
-        calculatedSubTotal: 250,
-        invoicedSubTotal: 215,
-        invoiceCredit: 5,
-        shipping: 20,
-        handling: 5,
-        taxes: 0,
-        total: 200,
-        discountAmount: 20,
-        discountType: 'PERCENT',
-      },{
-        id: '#KOM-123456789',
-        date: 'Feb 3, 2018',
-        currency: 'JPY',
-        calculatedSubTotal: 250,
-        invoicedSubTotal: 225,
-        invoiceCredit: 5,
-        shipping: 20,
-        handling: 5,
-        taxes: 0,
-        total: 200,
-        discountAmount: 20,
-        discountType: 'PERCENT',
-      }
-    ]
-    this.selectedInvoice = this.invoices[0];
+  constructor(
+    public reconcileService: ReconcileService
+  ) {
     this.board = {
       pkg: 'Box',
       qty: 20,
@@ -138,13 +53,18 @@ export class ReconcileComponent implements OnInit, OnDestroy {
   }
   
   ngOnInit() {
-
+    this.reconcileService.getInvoices().subscribe(res => {
+      this.invoices = res;
+      if (this.invoices.length > 0) {
+        this.selectedInvoice = this.invoices[0];
+      }
+    });
   }
   
   ngOnDestroy() {
     console.log('for unsubscribing')
   }
-  
+
   addSubscribers() {
     
   }
@@ -175,7 +95,7 @@ export class ReconcileComponent implements OnInit, OnDestroy {
 
   productSelect(product) {
     product.checked = !product.checked
-    this.panelVisible = any((pd) => pd.checked)(this.products);
+    this.panelVisible = any((pd) => pd.checked)(this.selectedInvoice.order_items);
   }
 
   bulkUpdates() {
