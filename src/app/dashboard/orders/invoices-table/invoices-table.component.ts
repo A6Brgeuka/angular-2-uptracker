dd45bd500b36e03994a1fac266dda14e944ab07e
tairezzzz <tairezzzz@gmail.com>
2018-04-17 12:59:53 +0300

    fix(orders:invoices-table): remove unnecessary import

--- src/app/dashboard/orders/invoices-table/invoices-table.component.ts
+++ src/app/dashboard/orders/invoices-table/invoices-table.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { DestroySubscribers } from 'ngx-destroy-subscribers';
 import { map } from 'lodash';
 
 import { OrdersService } from '../orders.service';
@@ -10,11 +9,9 @@ import { OrdersService } from '../orders.service';
   templateUrl: './invoices-table.component.html',
   styleUrls: ['./invoices-table.component.scss']
 })
-@DestroySubscribers()
 export class InvoicesTableComponent {
   public subscribers: any = {};
 
-  public visible: boolean[] = [];
   public invoicesTabs = {
     all: 'all',
     open: 'open',
