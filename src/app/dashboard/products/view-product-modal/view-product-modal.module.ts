ded8777d47079a5c6352a8ce49af437d7d40d61d
phoenix377 <phoenix.number377@gmail.com>
2018-04-19 17:53:24 +0300

    fix(marketplace:add-custom-product): Fix build issue

--- src/app/dashboard/products/view-product-modal/view-product-modal.module.ts
+++ src/app/dashboard/products/view-product-modal/view-product-modal.module.ts
@@ -4,12 +4,10 @@ import {ViewProductModal} from './view-product-modal.component';
 import {AppSharedModule} from '../../../shared/shared.module';
 import {InventoryDetailComponent} from './inventory-detail/inventory-detail.component';
 import {HistoryDetailComponent} from "./history-detail/history-detail.component";
-import {VariantDetailModule} from "../../../shared/components/variant-detail/variant-detail.module";
 
 @NgModule({
     declarations: [
         ViewProductModal,
-        VariantDetailModule,
         InventoryDetailComponent,
         HistoryDetailComponent
     ],
