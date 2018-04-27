342f4a3c3b42500397e24d91a85c88a33772b74c
tairezzzz <tairezzzz@gmail.com>
2018-04-27 18:42:45 +0300

    feat(orders:receive:models): add inventory_group control to order-item model

--- src/app/dashboard/orders/receive/models/order-item-form.model.ts
+++ src/app/dashboard/orders/receive/models/order-item-form.model.ts
@@ -21,7 +21,7 @@ export interface ReceiveOrderItemModel {
 
 export class OrderItemFormGroup extends FormGroup {
 
-  constructor({ id, inventory_group_id, status_line_items, quantity }: ReceiveOrderItemModel) {
+  constructor({ id, inventory_group_id, status_line_items, quantity, inventory_group }: ReceiveOrderItemModel) {
     const isStatusLineItemsArray = status_line_items && _.isArray(status_line_items);
     const filteredStatusLineItems = isStatusLineItemsArray ?
       status_line_items.filter((item) => !item.qty_change) : [];
@@ -30,6 +30,7 @@ export class OrderItemFormGroup extends FormGroup {
     super({
       id: new FormControl(id, Validators.required),
       inventory_group_id: new FormControl(inventory_group_id, Validators.required),
+      inventory_group: new FormControl(inventory_group),
       status: new FormArray([]),
       status_line_items: new FormArray(statusLineItemsFormGroups),
       quantity: new FormControl({value: quantity, disabled: true}),
