8b44468e2863ec1e03599444018730c2e859d86b
tairezzzz <tairezzzz@gmail.com>
2018-05-02 19:02:08 +0300

    fix(products/product/variant-short-detail): add value to price select

--- src/app/dashboard/products/product/variant-short-detail/variant-short-detail.component.ts
+++ src/app/dashboard/products/product/variant-short-detail/variant-short-detail.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { DestroySubscribers } from 'ngx-destroy-subscribers';
-import * as _ from 'lodash';
 
 import { AccountService } from '../../../../core/services/account.service';
 
 @Component({
   selector: 'app-variant-short-detail',
-  //TODO: [ngClass] here on purpose, no real use, just to show how to workaround ng2 issue #4330.
-  // Remove when solved.
-  /* tslint:disable */
   templateUrl: './variant-short-detail.component.html',
   styleUrls: ['./variant-short-detail.component.scss']
 })
 @DestroySubscribers()
 export class VariantShortDetailComponent implements OnInit {
   public locationArr: any;
+  public selectedPrice = 'unit_price';
 
-  @Input("variant") public variant;
-  @Input("product_id") public product_id;
-  @Input("showEdit") public showEdit;
+  @Input('variant') public variant;
+  @Input('product_id') public product_id;
+  @Input('showEdit') public showEdit;
 
   @Output() addToOrderWithVendor: EventEmitter<any> = new EventEmitter();
 
@@ -30,7 +27,7 @@ export class VariantShortDetailComponent implements OnInit {
 
 ) {
     this.subscribers.locationSubscribtion = this.accountService.locations$
-    .subscribe(r=>{this.locationArr = r});
+    .subscribe(r => {this.locationArr = r; });
 
   }
 
