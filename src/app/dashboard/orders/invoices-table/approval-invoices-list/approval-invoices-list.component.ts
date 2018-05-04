970d6ad24e56664cab47f262569f2fc8833fc736
tairezzzz <tairezzzz@gmail.com>
2018-05-04 19:11:40 +0300

    feat(orders:invoices-table): hide action menu

--- src/app/dashboard/orders/invoices-table/approval-invoices-list/approval-invoices-list.component.ts
+++ src/app/dashboard/orders/invoices-table/approval-invoices-list/approval-invoices-list.component.ts
@@ -28,7 +28,7 @@ export class ApprovalInvoicesListComponent implements OnInit, OnDestroy {
     {name: 'Reconciled by', className: 's2', alias: 'reconciled_by_name', filterBy: true, },
     {name: '# of Items', className: 's1 bold underline-text center-align', alias: 'item_count'},
     {name: 'Total', className: 's1 bold underline-text right-align', alias: 'total'},
-    // {name: '', className: 's1', actions: true},
+    {name: '', className: 's1', actions: false},
   ];
 
   public invoices$: Observable<Invoice[]>;
